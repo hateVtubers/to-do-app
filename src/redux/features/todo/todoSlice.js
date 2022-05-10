492f9602c1faff6e6a7c44990077c3bc4f60df5d
hateVtubers <gmolloflores@gmail.com>
2022-05-10 02:33:14 -0400

    add: localstorage for user experience

--- src/redux/features/todo/todoSlice.js
+++ src/redux/features/todo/todoSlice.js
@@ -1,24 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
+import uniqid from 'uniqid'
 
 const initialState = {
-	list: [],
+	list: JSON.parse(window.localStorage.getItem('todoList')) ?? [],
 };
 
-let id = 0;
 export const todoSlice = createSlice({
 	name: 'todo',
 	initialState,
 	reducers: {
 		addTodo: (state, action) => {
-			state.list.push({ name: action.payload, id: id++ });
+			state.list.push({ name: action.payload, id: uniqid(action.payload) });
+      window.localStorage.setItem('todoList', JSON.stringify(state.list));
 		},
 		deleteTodo: (state, action) => {
 			const todoId = action.payload;
 			const todosFiltered = state.list.filter(todo => todo.id !== todoId);
 			state.list = todosFiltered;
+
+      window.localStorage.setItem('todoList', JSON.stringify(state.list));
 		},
+    updateTodo: (state, { payload }) => {
+      const todosUpdated = state.list.map(({ id, ...res }) =>
+        id === payload.id ? { id, name: payload.name } : { ...res, id }
+      )
+
+      state.list = todosUpdated
+      window.localStorage.setItem('todoList', JSON.stringify(todosUpdated))
+    },
 	},
 });
 
-export const { addTodo, deleteTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, updateTodo } = todoSlice.actions;
 export default todoSlice.reducer;
