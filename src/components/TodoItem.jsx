f66c1366c82d8205811a3c9e300bba9ba895ce94
hateVtubers <gmolloflores@gmail.com>
2022-05-10 02:33:49 -0400

    todo: update task added

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,45 @@
+// @ts-check
 import { useDispatch } from 'react-redux';
-import { deleteTodo } from '../redux/features/todo/todoSlice';
+import { useState } from 'react';
+import { deleteTodo, updateTodo } from '../redux/features/todo/todoSlice';
 
 const TodoItem = ({ name, id }) => {
 	const dispatch = useDispatch();
+  const [editting, setEditting] = useState(false);
+
 	return (
 		<li className='list-group-item d-flex justify-content-between align-items-center'>
-			<span>{name}</span>
-			<button
-				type='button'
-				className='btn btn-danger'
-				onClick={() => dispatch(deleteTodo(id))}
-			>
-				X
-			</button>
+			{editting ? (
+        <input
+          type='text'
+          defaultValue={name}
+					className='form-control w-50'
+          autoFocus
+          onBlur={(e) => { // unfocus, https://reactjs.org/docs/events.html#onblur
+						dispatch(updateTodo({ name: e.currentTarget.value, id }))
+						setEditting(!editting)
+					}}
+        />
+      ) : (
+        <span>{name}</span>
+      )}
+			<div>
+        <button
+          type='button'
+          className='btn btn-warning text-white mr-5'
+          style={{ marginRight: '5px' }}
+          onClick={() => setEditting(!editting)}
+        >
+          rename
+        </button>
+        <button
+          type='button'
+          className='btn btn-danger'
+          onClick={() => dispatch(deleteTodo(id))}
+        >
+          X
+        </button>
+      </div>
 		</li>
 	);
 };
